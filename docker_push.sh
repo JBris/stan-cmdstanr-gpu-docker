54d920eb1ea47b14bc29ed98884da278845e18b2
James Bristow <JBris@users.noreply.github.com>
2024-08-07 10:58:33 +1200

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -6,8 +6,4 @@
 # Main
 ###################################################################
 
-echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
-
-DOCKER_IMAGE_HASH=$(docker images --format "{{.ID}} {{.CreatedAt}}" | sort -rk 2 | awk 'NR==1{print $1}')
-docker tag "$DOCKER_IMAGE_HASH" ghcr.io/jbris/stan-cmdstanr-gpu-docker:${CMDSTAN_VERSION}
 docker push ghcr.io/jbris/stan-cmdstanr-gpu-docker:${CMDSTAN_VERSION}
