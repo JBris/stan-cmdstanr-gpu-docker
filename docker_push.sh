019e69a4a8159789e61c3001eda307c93406c314
James Bristow <JBris@users.noreply.github.com>
2024-08-07 10:58:44 +1200

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -6,4 +6,4 @@
 # Main
 ###################################################################
 
-docker push ghcr.io/jbris/stan-cmdstanr-gpu-docker:${CMDSTAN_VERSION}
+docker compose push ghcr.io/jbris/stan-cmdstanr-gpu-docker:${CMDSTAN_VERSION}
